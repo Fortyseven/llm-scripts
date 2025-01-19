c529e5ea4d3b87397b7efb1b8c417585be05dbeb
Fortyseven <fortyseven@network47.org>
2025-01-19 18:21:39 -0500

    feat: style refinements and error capturing

--- llm-cmd.py
+++ llm-cmd.py
@@ -6,18 +6,24 @@
 # and found one exactly like what I was about to do. So let's
 # build on it.
 
-DEBUG=True
+DEBUG=False
 
 SPROMPT="""
-You are a world-class Linux administrator. You will be asked to provide a command that will run in a Ubuntu Linux desktop terminal. Provide the complete command, then an explanation for what each argument does, and any notes that might help give context for the command.
+You are a world-class Linux administrator. You will be asked to provide a command that will run in a Ubuntu Linux desktop terminal. Provide the complete command, then an explanation for what each argument does, and any notes that might help give context for the command. Keep responses concise and to the point.
+
+Do not invent commands or arguments. Use only those that you are certain are available in the Ubuntu Linux desktop terminal.
+
+If you cannot complete the request, leave everything else blank, but only provide an error message.
 """
 
 # MODEL="llama3.1"
-MODEL="mistral-small:latest"
+# MODEL="mistral-small:latest"
+MODEL="llama3.2"
 OPTIONS=""
 
 import sys
 import json
+from typing import Optional
 import ollama
 from rich import print
 from pydantic import BaseModel
@@ -34,6 +40,7 @@ class CmdResponse(BaseModel):
     # command_explanation: str
     notes: str
     example_usage: str
+    error_message: Optional[str]
 
 args = " ".join(sys.argv[1:])
 
@@ -71,17 +78,17 @@ try:
 
     response = json.loads(response)
 
-    table = Table(show_header=False, padding=(0, 0))
+    table = Table(show_header=False, padding=(0, 0), show_lines=False)
 
     # table.add_column("Command", style="cyan")
 
 
-    if 'command' in response:
-        # print(f"[yellow][bold]Command:[/bold] {response['command']}[/yellow]\n")
+    if 'command' in response and response['command'] != "":
         table.add_row("Command", f"[yellow]{response['command']}[/yellow]")
+        table.add_section()
 
     # iterate each argument in the arg_explanation array
-    if 'arg_explanation' in response:
+    if 'arg_explanation' in response and len(response['arg_explanation']):
         subtable = Table(show_header=False)
         subtable.add_column("Argument", style="cyan")
         subtable.add_column("Description", style="cyan")
@@ -90,15 +97,19 @@ try:
                 subtable.add_row(explainer['arg'], explainer['description'])
         table.add_row("Arguments", subtable)
 
+        table.add_section()
 
 
-    if 'notes' in response:
-        # print(f"[purple][bold]Notes:[/bold] {response['notes']}[/purple]")
+
+    if 'notes' in response and response['notes'] != "":
         table.add_row("Notes", f"[purple]{response['notes']}[/purple]")
+        table.add_section()
 
-    if 'example_usage' in response:
-        # print(f"[green][bold]Example Usage:[/bold] {response['example_usage']}[/green]")
+    if 'example_usage' in response and response['example_usage'] != "":
         table.add_row("Example Usage", f"[green]{response['example_usage']}[/green]")
+
+    if 'error_message' in response and response['error_message'] != "":
+        table.add_row("Error", f"[red]{response['error_message']}[/red]")
 
     console = Console()
     console.print(table)
