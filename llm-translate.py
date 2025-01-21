77eb9aa8909af2f23831df87f3eafc5ee47ebb73
Fortyseven <fortyseven@network47.org>
2025-01-21 02:38:29 -0500

    feat: complete overhaul of the translation script using structured JSON output

--- llm-translate.py
+++ llm-translate.py
@@ -2,73 +2,144 @@
 
 import sys
 import json
+from typing import Optional
 import ollama
 from rich import print
+from pydantic import BaseModel
+from rich.table import Table
+from rich.console import Console
 
-MODEL='llama3.2:latest'
-#MODEL='phi3:latest'
-JSON_TEMPLATE = """
-{
-    "src_language":"$LANGUAGE",
-    "translation": "$TRANSLATION",
-    "src_pronunciation":"$SRC_PRONUNCIATION",
-    "notes":"$NOTES"
-}
-"""
+import argparse
 
-PROMPT=f"""
-You are a world class language translator.
+DEBUG = False
+
+args = None
+
+
+# This script borrows the filename + system prompt of another
+# tool already called "llm-cmd". I went looking for a good prompt
+# and found one exactly like what I was about to do. So let's
+# build on it.
+
+SPROMPT = """You are a world class language translator.
 
 Translate the provided text into English. Translate the entire provided text.
 
-If you cannot confidently and correctly translate the text, please respond with "I cannot confidently translate this text."
+- Provide the complete translation of the text.
+- Provide the language of the original text.
+- Provide a simple pronunciation of the translated text for native English speakers.
+- Provide any notes that might help give context for the translation.
+- Break down each part of the text and provide a translation for each part.
+- Do not invent or guess at a word's meaning. If you are unsure of a word's meaning, you may provide a literal translation of the word.
+- Keep responses concise and to the point.
+- Provide notes that might help give context for the translation.
+{}
 
-Do not invent or guess at a word's meaning. If you are unsure of a word's meaning, you may provide a literal translation of the word.
+You may leave any of these blank if they are not applicable or helpful.
 
-Provide a simple pronunciation of the translated text for native English speakers. If you cannot provide a pronunciation, leave this field blank.
-
-Also add notes that might help give context for the translation.
-
-Respond ONLY with a properly structured JSON object (don't forget to escape any quotation marks) exactly like this:
-
-{JSON_TEMPLATE}\n
+If you cannot confidently and correctly translate the text, leave everything else blank, but only provide an error message, such as "I cannot confidently translate this text."
 """
-args = " ".join(sys.argv[1:])
 
-if len(args) == 0:
-    print("Usage: translate.py <text>")
-    sys.exit(1)
-
-response = ollama.chat(
-    model=MODEL,
-    messages=[
-        {
-            "role": "user",
-            "content": PROMPT + args
-        }
-    ],
-    # stream=True,
-    options={
-        'temperature': 0.2,
-    },
+PROMPT_BREAKDOWN = (
+    "- Break down each part of the text and provide a translation for each part."
 )
 
+# MODEL="llama3.1"
+# MODEL = "mistral-small:latest"
+MODEL = "deepseek-r1:14b"
+# MODEL = "llama3.2"
+OPTIONS = ""
+TEMPERATURE = 0.15
+NUM_CTX = 2048
+
+
+class BreakdownPart(BaseModel):
+    part: str
+    translation: str
+
+
+class Translation(BaseModel):
+    english_translation: str
+    language: str
+    notes: str
+    breakdown: Optional[list[BreakdownPart]]
+    error_message: Optional[str]
+
+
+parser = argparse.ArgumentParser(description="Translate text into English.")
+# parser.add_argument(
+#     "--breakdown", help="Break down each word in the translation.", action="store_true"
+# )
+parser.add_argument("text", nargs="*", help="The text to translate.", type=str)
+args = parser.parse_args()
+
+args.breakdown = True  # HACK
+
+
+sprompt = SPROMPT.format(PROMPT_BREAKDOWN if args.breakdown else "")
+response = None
 try:
-    response = response.get('message').get('content')
+    response = ollama.chat(
+        model=MODEL,
+        messages=[
+            {
+                "role": "system",
+                "content": sprompt,
+            },
+            {"role": "user", "content": " ".join(args.text)},
+        ],
+        options={"temperature": TEMPERATURE, "num_ctx": NUM_CTX},
+        format=Translation.model_json_schema(),
+    )
+
+    response = response.get("message").get("content")
 
     # sanitize
-    response = response.replace("”", "\"")
-    response = response.replace("“", "\"")
+    response = response.replace("”", '"')
+    response = response.replace("“", '"')
     response = response.replace("’", "'")
     response = response.replace("‘", "'")
 
     response = json.loads(response)
 
-    print(f"[red]LANG:[/red] {response['src_language']} -> [green]\"{response['translation']}\"[/green]")
-    if response['src_pronunciation']:
-        print(f"[purple]SRC_PRONUNCIATION: [bold]{response['src_pronunciation']}[/bold][/purple]")
-    if response['notes']:
-        print(f"[yellow]NOTES: {response['notes']}[/yellow]")
+    table = Table(show_header=False, padding=(0, 1), show_lines=False, show_edge=False)
+
+    if "english_translation" in response and response["english_translation"] != "":
+        table.add_row(
+            "Translation", f"[yellow]{response['english_translation']}[/yellow]"
+        )
+        table.add_section()
+
+    if "language" in response and response["language"] != "":
+        table.add_row("Language", f"[cyan]{response['language']}[/cyan]")
+        table.add_section()
+
+    if "breakdown" in response and response["breakdown"] != "":
+        subtable = Table(
+            show_header=False, show_lines=False, padding=(0, 1), show_edge=False
+        )
+        subtable.add_column("Part", style="cyan")
+        subtable.add_column("Translation", style="yellow")
+        for part in response["breakdown"]:
+            subtable.add_row(part["part"], part["translation"])
+        table.add_row("Breakdown", subtable)
+        table.add_section()
+
+    if "notes" in response and response["notes"] != "":
+        table.add_row("Notes", f"[green]{response['notes']}[/green]")
+        table.add_section()
+
+    if "error_message" in response and response["error_message"] != "":
+        table.add_row("Error", f"[red]{response['error_message']}[/red]")
+
+    console = Console()
+    console.print(table)
+
+    if DEBUG:
+        print(sprompt)
+        print()
+        print(console.print(response))
+
 
 except Exception as e:
-    print(f"ERROR: {e}\n\n{response}")
+    print(f"ERROR: {e}\n{response if response else ''}")
